4f53e7c4abbb68c525e8eeea220538a531d4262e
Christian Boyle <mail@christianboyle.com>
2024-07-25 07:39:43 -0500

    replace DOMNodeRemoved with mutation observer

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
-window.addEventListener('DOMNodeRemoved', function() {
-  if (document.getElementById('darkstyle_css') == null) {
-    var head = document.getElementsByTagName('head')[0],
-      link = document.createElement('link');
+const observer = new MutationObserver(mutationCallback);
 
-    link.id     = 'darkstyle_css';
-    link.rel    = 'stylesheet';
-    link.type   = 'text/css';
-    link.href   = chrome.extension.getURL('css_dark.css');
-    link.media  = 'all';
-
-    head.appendChild(link);
+function mutationCallback(mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      if (!document.getElementById('darkstyle_css')) {
+        addDarkStyleSheet();
+      }
+    }
   }
-});
+}
 
+function addDarkStyleSheet() {
+  const head = document.getElementsByTagName('head')[0];
+  const link = document.createElement('link');
+
+  link.id = 'darkstyle_css';
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = chrome.extension.getURL('css_dark.css');
+  link.media = 'all';
+
+  head.appendChild(link);
+}
+
+observer.observe(document.documentElement, { childList: true, subtree: true });
